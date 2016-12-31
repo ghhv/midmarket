fde263b091730eeac3ecc2a5080d361273e3e582
Max Heinritz <maxheinritz@gmail.com>
2016-12-30 17:24:18 -0800

    initial commit

---
+++ src/places.js
@@ -0,0 +1,45 @@
+export default [
+    {
+        "id": "trinity_place_4",
+        "title": "Trinity Place (Phase 4)",
+        "lat": 37.778749,
+        "lng": -122.41396,
+        "link": "http://www.trinitysf.com/trinity-place/",
+        "description": "The final part of a large apartment complex with public open spaces and retail.",
+        "address": "1188 Mission St",
+        "eta": "2018+",
+        "rooms": 502,
+        "retail": 45000
+    },
+    {
+        "id": "trinity_place_3",
+        "title": "Trinity Place (Phase 3)",
+        "lat": 37.778357,
+        "lng": -122.413391,
+        "link": "http://www.trinitysf.com/trinity-place/",
+        "description": "The third part of a large apartment complex with public open spaces and retail. Features a large metal statue in a courtyard.",
+        "eta": "Q2 2017",
+        "rooms": 541
+    },
+    {
+        "id": "proper_hotel",
+        "title": "Proper Hotel",
+        "lat": 37.780923,
+        "lng": -122.412479,
+        "link": "http://www.properhotel.com/san-francisco",
+        "description": "The SF location of the Kor Group's a new \"Proper\" hotel brand.",
+        "address": "45 McAllister S",
+        "eta": "Q4 2017",
+        "rooms": 203
+    },
+    {
+        "id": "yotel",
+        "title": "YOTEL",
+        "lat": 37.78038,
+        "lng": -122.412136,
+        "link": "http://synapsed.com/portfolio/yotel-sanfrancisco/",
+        "description": "The SF debut of YOTEL, a hotel brand inspired by the modernity and efficiency of Japanese capsule hotels.",
+        "address": "1095 Market Street",
+        "eta": "Q4 2017"
+    }
+];
