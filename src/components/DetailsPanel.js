6fadc049597bde2e126e18c1054552b332f86038
Max Heinritz <maxheinritz@gmail.com>
2017-01-07 22:08:29 -0800

    nit ===

--- src/components/DetailsPanel.js
+++ src/components/DetailsPanel.js
@@ -74,7 +74,7 @@ class DetailsPanel extends Component {
       this._autoScrollCount++;
       scrollTo(this.cardTops[this.props.selectedIndex], () => {
         this._autoScrollCount--;
-        if (this._autoScrollCount == 0) {
+        if (this._autoScrollCount === 0) {
           // HACK: the callback is called before the scroll finishes,
           // so add delay...
           setTimeout(() => this.setScrollHandlerEnabled(true), 100);
